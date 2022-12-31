d06dab2fc77f72a9a74026f4a75165b81631196a
iisaiaso <isaiasibarraonofre@gmail.com>
2022-12-31 02:15:45 -0500

    Trabajo culminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageModule } from './page/page.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -6,10 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-nombre!:string
-apellido!:string
-edad!:string
-capturar(nam:string, ape:string, eda:string){
 
-}
+  n!: string
+  a!: string
+  e!: string
+  data: any[] = []
+  mensaje!: string
+
+  capturar(nom: string, ape: string, edad: string) {
+    this.n = nom
+    this.a = ape
+    this.e = edad
+    /* Para generar un objeto */
+    class agregarPersona {
+      nombre: string
+      apellido: string
+      edad: string
+
+      constructor(nom: string, ape: string, age: string) {
+        this.nombre = nom
+        this.apellido = ape
+        this.edad = age
+      }
+
+    }
+    /** */
+    var persona = new agregarPersona(this.n, this.a, this.e)
+    this.data.push(persona)
+    // if (this.e >= "18") {
+    //   this.mensaje = this.n + " " + this.a + " " + "Es mayor"
+    // }else{
+    //   this.mensaje = this.n + " " + this.a + " " + "Es menor"
+    // }
+    console.log(persona)
+  }
+
 }
